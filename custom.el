1534d98ece1615841c1acc9f714ac020b13af5d7
Radu Butoi <rbutoi@gmail.com>
2016-06-20 10:01:21 -0700

    add custom.el, reorganize, small fixes

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compilation-message-face (quote default))
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(diff-switches "-u")
+ '(indent-tabs-mode nil)
+ '(ns-command-modifier (quote control))
+ '(org-journal-file-format "%Y%m%d.txt")
+ '(recentf-max-saved-items 100)
+ '(recentf-save-file "~/.emacs.d/recentf")
+ '(vc-annotate-background nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
