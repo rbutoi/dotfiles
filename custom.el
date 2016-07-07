ecd34b0a2d15d1ffbdd13efbd52d836abca30547
Radu Butoi <rbutoi@gmail.com>
2016-07-07 12:56:33 -0700

    custom

--- custom.el
+++ custom.el
@@ -6,11 +6,14 @@
  '(custom-safe-themes
    (quote
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(indent-tabs-mode nil)
+ '(large-file-warning-threshold nil)
  '(safe-local-variable-values (quote ((eval switch-org-and-elisp-map 1)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(which-func ((t (:foreground "dark cyan")))))
 
