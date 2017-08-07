d613d38ee7ab82ca774c63a1448653210a4b7997
Radu Butoi <butoi@butoi.mtv.corp.google.com>
2017-08-07 10:10:07 -0700

    GOOG changes

--- custom.el
+++ custom.el
@@ -9,4 +9,15 @@
  '(gud-gdb-command-name "gdb --annotate=1")
  '(indent-tabs-mode nil)
  '(large-file-warning-threshold nil)
+ '(minimap-dedicated-window nil)
+ '(minimap-mode nil)
+ '(package-selected-packages
+   (quote
+    (ztree zoom-window yafolding which-key visual-fill-column undo-tree term-run rust-mode rich-minority pkgbuild-mode pallet org-journal nlinum mwim magit keychain-environment itail helm-unicode helm-swoop helm-descbinds helm-ag haskell-mode golden-ratio flycheck-rust dtrt-indent dired+ diffview company-anaconda color-theme-solarized buffer-move ag ack ace-window ace-isearch)))
  '(safe-local-variable-values (quote ((eval switch-org-and-elisp-map 1)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
