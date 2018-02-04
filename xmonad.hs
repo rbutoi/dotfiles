3a8a4f5176f217d309074efc2e08c54843879ab1
Radu Butoi <rbutoi@gmail.com>
2018-02-04 00:39:57 -0500

    various VM updates

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,9 @@
 import XMonad
-import XMonad.Actions.CycleWS
 import XMonad.Config.Xfce
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
 
 import qualified XMonad.StackSet as W
 
@@ -11,7 +11,7 @@ main = xmonad $ xfceConfig
     { modMask = mod1Mask
     , handleEventHook = fullscreenEventHook
     , terminal = "xfce4-terminal"
-    , borderWidth = 6
+    , borderWidth = 3
     , workspaces = map show [1..4]
     , focusedBorderColor = "#20B9A5"
     } `additionalKeysP`
@@ -19,16 +19,19 @@ main = xmonad $ xfceConfig
     , ("M-k", windows W.focusDown)
     , ("M-S-j", windows W.swapUp)
     , ("M-S-k", windows W.swapDown)
+    , ("M-<F4>", kill)
     , ("M-S-n", nextWS)
     , ("M-S-p", prevWS)
     , ("M-C-,", sendMessage (IncMasterN 1)) -- for the almighty emacs
     , ("M-C-.", sendMessage (IncMasterN (-1)))
-    , ("M-S-e", spawn "emacsclient -c -a emacs")
+    -- xfce
+    -- , ("M-S-e", spawn "emacsclient -c -a emacs")
     , ("M-S-b", sendMessage ToggleStruts)
     , ("M-S-m", windows W.swapMaster)
-    , ("M-S-h", spawn "/home/radu/.local/bin/hidpi_mode.sh")
-    , ("M-C-<Return>", spawn "xfce4-terminal -e \"sh -c 'tmux a -d || tmux'\"")
+    -- let xfce handle it, so I can use it with xfwm4 as well
+    -- , ("M-C-<Return>", spawn "xfce4-terminal -e \"sh -c 'tmux a -d || tmux'\"")
     , ("M-S-q", spawn "xmonad --recompile && xmonad --restart")
+    , ("C-M-S-q", spawn "killall xmonad-x86_64-linux && xfwm4 --daemon --replace")
     ] `removeKeysP`
     [ ("M-q"),("M-w"),("M-e"),("M-b"),("M-p"),("M-n")
     ,("M-1"),("M-2"),("M-3"),("M-4"),("M-5"),("M-6"),("M-7"),("M-8"),("M-9")
