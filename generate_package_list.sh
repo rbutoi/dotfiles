fb69ed742727427dadf359c808166204ae45e10b
Radu Butoi <rbutoi@gmail.com>
2018-03-14 10:50:15 -0400

    upd8 packages

--- generate_package_list.sh
+++ generate_package_list.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-comm <(pacman -Qqt | sort) <(pacman -Qg | cut -d' ' -f2 | sort) -23 > package_list.txt
+comm <(pacman -Qqe | sort) <(pacman -Qg | cut -d' ' -f2 | sort) -23 > package_list.txt
