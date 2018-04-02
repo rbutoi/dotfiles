3b60585adcb1a322ba3e169140102171acdd0294
Radu Butoi <rbutoi@gmail.com>
2018-04-02 15:42:56 -0400

    package list per OS

--- generate_package_list.sh
+++ generate_package_list.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-comm <(pacman -Qqe | sort) <(pacman -Qg | cut -d' ' -f2 | sort) -23 > package_list.txt
+
+os=$(lsb_release -si)
+outname=package_list_$(lsb_release -sd | sed 's/"//g' | sed 's/[ /]/_/g')
+
+case $os in
+Arch)
+    comm <(pacman -Qqe | sort) <(pacman -Qg | cut -d' ' -f2 | sort) -23 > $outname.txt
+    ;;
+Raspbian)
+    apt-mark showmanual > $outname.txt
+    ;;
+esac
