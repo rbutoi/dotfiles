a681a22b0a281a342229aa9a5a3e93153ce81682
Radu Butoi <rbutoi@gmail.com>
2018-04-07 17:09:49 -0400

    .zile

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.emacs.d
 ln -sf $PWD/.bashrc      ~
 ln -sf $PWD/.screenrc    ~
 ln -sf $PWD/.tmux.conf   ~
+ln -sf $PWD/.zile        ~
 ln -sf $PWD/settings.org ~/.emacs.d/
 ln -sf $PWD/custom.el    ~/.emacs.d/
 cp  -f $PWD/init.el      ~/.emacs.d/
