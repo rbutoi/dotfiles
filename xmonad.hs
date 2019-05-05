140c314743a25f5bf26480cefb617fa687e49b74
Radu Butoi <rbutoi@gmail.com>
2019-05-05 16:31:06 -0400

    update xmonad for new computer

--- xmonad.hs
+++ xmonad.hs
@@ -6,16 +6,22 @@ import XMonad.Actions.CycleWS
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect
 
 import qualified DBus as D
 import qualified DBus.Client as D
 import qualified XMonad.StackSet as W
 
+modMask' = mod1Mask
+
 myStartupHook = do
   startupHook xfceConfig
-  spawn "compton -b"
+  spawn "compton -CGb"
 
-modMask' = mod1Mask
+myManageHook = composeAll
+   [ title =? "Whisker Menu"  --> doFloat
+   , manageDocks
+   ]
 
 main :: IO ()
 main = do
@@ -26,9 +32,9 @@ main = do
 
   xmonad $ xfceConfig
     { modMask = modMask'
-    , manageHook = manageDocks <+> manageHook xfceConfig
+    , manageHook = myManageHook <+> manageHook xfceConfig
       -- no borders needed, just updatePointer
-    , layoutHook = noBorders $ desktopLayoutModifiers $ Tall 1 (3/100) (1/2) ||| Full
+    , layoutHook = noBorders $ desktopLayoutModifiers $ reflectHoriz $ Tall 1 (3/100) (1/2) ||| Full
     , logHook    = ewmhDesktopsLogHook <+> logHook xfceConfig
       -- haskell syntax is so wat:
                    >> updatePointer (0.5, 0.5) (0, 0)
@@ -39,29 +45,33 @@ main = do
     , workspaces = map show [1..4]
     , focusedBorderColor = "#20B9A5"
     } `additionalKeysP`
-    [ ("M-j", windows W.focusUp)
-    , ("M-k", windows W.focusDown)
-    , ("M-S-j", windows W.swapUp)
-    , ("M-S-k", windows W.swapDown)
+    [ ("M-k", windows W.focusUp)
+    , ("M-j", windows W.focusDown)
+    , ("M-h", sendMessage Expand)
+    , ("M-l", sendMessage Shrink)
+    , ("M-S-k", windows W.swapUp)
+    , ("M-S-j", windows W.swapDown)
     , ("M-<F4>", kill)
     , ("M-S-n", nextWS)
     , ("M-S-p", prevWS)
       -- for the almighty emacs
     , ("M-C-,", sendMessage (IncMasterN (-1)))
     , ("M-C-.", sendMessage (IncMasterN   1))
-    -- xfce
-    -- , ("M-S-e", spawn "emacsclient -c -a emacs")
     , ("M-S-b", sendMessage ToggleStruts)
-    , ("M-S-m", windows W.swapMaster)
-    -- let xfce handle it, so I can use it with xfwm4 as well
-    -- , ("M-C-<Return>", spawn "xfce4-terminal -e \"sh -c 'tmux a -d || tmux'\"")
+    , ("M-m", nextScreen)
+    , ("M-S-m", windows W.focusMaster)
+    , ("C-M-S-m", windows W.swapMaster)
     , ("M-S-q", spawn "xmonad --recompile && xmonad --restart")
     , ("C-M-S-q", spawn "killall xmonad-x86_64-linux && xfwm4 --daemon --replace")
-    , ("M-m", nextScreen)
+    , ("M-S-<Return>"  , spawn "xfce4-terminal -e \"sh -c 'tmux a -d || tmux'\"")
+    , ("M-<Return>", spawn "xfce4-terminal")
+    , ("M-S-e", spawn "emacsclient -c -a emacs")
+    , ("M-`", spawn "xfce4-popup-whiskermenu")
+    , ("M-S-f", spawn "firefox")
     ] `removeKeysP`
     [ ("M-q"),("M-w"),("M-e"),("M-b"),("M-p"),("M-n")
     ,("M-1"),("M-2"),("M-3"),("M-4"),("M-5"),("M-6"),("M-7"),("M-8"),("M-9")
-    ,("M-<Return>"),("M-,"),("M-."),("M-S-/"),("M-S-e"),("M-S-r")
+    ,("M-,"),("M-."),("M-S-/"),("M-S-r")
     ] `additionalMouseBindings`
     [ ((modMask', 9), (\_ -> prevWS))
     , ((modMask', 8), (\_ -> nextWS))
