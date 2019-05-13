e6f3ddd72ace8b04ace506a641c233e98c8c3c4c
Radu Butoi <rbutoi@gmail.com>
2019-05-13 05:02:40 -0400

    line width

--- generate_package_list.sh
+++ generate_package_list.sh
@@ -5,8 +5,9 @@ outname=package_list_$(lsb_release -sd | sed 's/"//g' | sed 's/[ /]/_/g')
 
 case $os in
 Arch)
-#                                           remove commonly-install packaage groups
-    comm <(pacman -Qqe | sort) <(pacman -Qg base base-devel xorg xorg-fonts | cut -d' ' -f2 | sort) -23 > $outname.txt
+# remove commonly-install packaage groups
+    comm > $outname.txt <(pacman -Qqe | sort) \
+         <(pacman -Qg base base-devel xorg xorg-fonts | cut -d' ' -f2 | sort) -23
     ;;
 Raspbian)
     apt-mark showmanual > $outname.txt
