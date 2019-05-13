6521c2cdbcd02fd896c2f8d0ace2eea8bb91354b
Radu Butoi <rbutoi@gmail.com>
2019-05-13 04:28:51 -0400

    don't remove *all* packages in groups from package listing
    just common ones

--- generate_package_list.sh
+++ generate_package_list.sh
@@ -5,7 +5,8 @@ outname=package_list_$(lsb_release -sd | sed 's/"//g' | sed 's/[ /]/_/g')
 
 case $os in
 Arch)
-    comm <(pacman -Qqe | sort) <(pacman -Qg | cut -d' ' -f2 | sort) -23 > $outname.txt
+#                                           remove commonly-install packaage groups
+    comm <(pacman -Qqe | sort) <(pacman -Qg base base-devel xorg xorg-fonts | cut -d' ' -f2 | sort) -23 > $outname.txt
     ;;
 Raspbian)
     apt-mark showmanual > $outname.txt
