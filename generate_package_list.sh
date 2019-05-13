e5686d1fcd01f9d58e0ee3d398c2ba6c2d9267ab
Radu Butoi <rbutoi@gmail.com>
2019-05-13 05:03:33 -0400

    why push

--- generate_package_list.sh
+++ generate_package_list.sh
@@ -5,7 +5,7 @@ outname=package_list_$(lsb_release -sd | sed 's/"//g' | sed 's/[ /]/_/g')
 
 case $os in
 Arch)
-# remove commonly-install packaage groups
+    # remove commonly-installed package groups
     comm > $outname.txt <(pacman -Qqe | sort) \
          <(pacman -Qg base base-devel xorg xorg-fonts | cut -d' ' -f2 | sort) -23
     ;;
