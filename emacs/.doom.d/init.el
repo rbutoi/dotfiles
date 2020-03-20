97f2316e4b6026ee6c6714abc033581dce54401e
Radu Butoi <rbutoi@gmail.com>
2020-03-20 11:05:05 -0400

    remove perl/python, defaults emacs modes are fine for me

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -143,11 +143,11 @@
         ;;+pandoc          ; export-with-pandoc support
         ;;+pomodoro        ; be fruitful with the tomato technique
         +present)        ; using org-mode for presentations
-       perl              ; write code no one else can comprehend
+       ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
