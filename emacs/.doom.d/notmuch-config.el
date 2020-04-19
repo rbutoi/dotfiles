aea37efb14a604af72590bb73b94e53924a0db9d
Radu Butoi <butoi@google.com>
2020-04-19 02:40:14 -0400

    echo browse-URLs. mv notmuch config to its file
    and ztree is nice, even if no binds

---
+++ emacs/.doom.d/notmuch-config.el
@@ -0,0 +1,8 @@
+;;; notmuch-config.el -*- lexical-binding: t; -*-
+
+(setq
+ notmuch-search-oldest-first nil) ; why does this default to t
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
