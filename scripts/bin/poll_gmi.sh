1d51a638fcb58678a4ffcd481dc62970596ba451
Radu Butoi <butoi@google.com>
2020-06-08 11:15:13 -0400

    run in parallel, not quiet since crontab silences anyway
    installing python / dealing with paths on mac is very annoying

--- scripts/bin/poll_gmi.sh
+++ scripts/bin/poll_gmi.sh
@@ -2,12 +2,13 @@
 
 # macOS compat
 if command -v brew >/dev/null; then
-  export PYTHONPATH="$(brew --prefix)/lib/python3.7/site-packages:$PYTHONPATH"
+  export PYTHONPATH="$(brew --prefix)/lib/python3.8/site-packages:$PYTHONPATH"
 fi
 
 command -v gmi >/dev/null && GMI=gmi || GMI=~/bin/gmi
 
-[ -e ~/.mail/work ] && $GMI sync -q -C ~/.mail/work
-$GMI sync -q -C ~/.mail/personal
+[ -e ~/.mail/work ]     && $GMI sync -C ~/.mail/work     &
+[ -e ~/.mail/personal ] && $GMI sync -C ~/.mail/personal &
+wait
 
-notmuch new --quiet # to run hooks
+notmuch new # to run hooks
