7e5062190ea09a8300fcde80725b9dcf3deaccdd
Radu Butoi <butoi@google.com>
2021-04-14 10:09:31 -0400

    waybar battery limiter toggling with notif

---
+++ scripts/bin/battery-toggle-full-or-limit-charge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cap=`cat /sys/class/power_supply/BAT0/capacity`
+
+if [ `sudo -A /usr/local/bin/tpacpi-bat -g ST 1 | awk '{print $1}'` == 0 ] &&
+   [ `sudo -A /usr/local/bin/tpacpi-bat -g SP 1 | awk '{print $1}'` == 0 ]; then
+  battery-set-normal-charge-levels
+  status=`cat /sys/class/power_supply/BAT0/status`
+  notify-send "$cap%: $status (limited charging)" "$(battery-get-charge-levels)"
+else
+  battery-charge-full
+  status=`cat /sys/class/power_supply/BAT0/status`
+  notify-send "$cap%: $status"                    "charging to full"
+fi
