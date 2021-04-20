be33b17aeb3c77579d6d4045089a88ff55ff057e
Radu Butoi <butoi@google.com>
2021-04-20 10:52:45 -0400

    kill wob
    has been OOM-killed numerous times and is nothing but trouble, keeps
    dying and leaves many processes around trying to write to the sock. use
    mako instead

--- sway/bin/make_pipe_run_wob.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pkill -f "tail -f $SWAYSOCK.wob" # also kills the wob it feeds it seems
-
-rm -f $SWAYSOCK.wob
-mkfifo $SWAYSOCK.wob
-tail -f $SWAYSOCK.wob 2>~/.cache/.wob_tail_errors | wob -O* 2> ~/.cache/.wob_errors
