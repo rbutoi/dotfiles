cd38f77197eaf64f78324eee10c2d48b5a401dab
Radu Butoi <rbutoi@gmail.com>
2021-06-21 16:21:17 -0400

    autocompile/install updates
    +eless promising, exactly what I wanted to do forever (| emacs)
    +fish and rustup{,-git} conflict on bash completion? and I had them installed before together

---
+++ scripts/bin/sysupdate_compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Automatically update & install AUR/devel packages (and Emacs, requiring an
+# elisp compilation). Should™ be safe since it only updates non-core/system
+# packages.
+
+. ~/.config/path.sh # need to set PATH for Doom
+
+if ! load_below; then
+  echo system load over threshold, not updating
+  exit 0
+fi
+
+paru -Sua --devel --noconfirm
+
+doom -y upgrade
+# libvterm needs recompiling after Emacs (scraped from `vterm.el`)
+cd ~/.emacs.d/.local/straight/build-*/vterm/; mkdir -p build; cd build
+cmake -G 'Unix Makefiles'  ..
+make
