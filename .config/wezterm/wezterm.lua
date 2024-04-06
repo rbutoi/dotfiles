780da9070f7ddf99c6d0d56a53ba7b2839fb9ffc
Radu Butoi <rbutoi@gmail.com>
2024-04-06 17:40:14 -0400

    radical wez simplifications / job, including s/"/'/
    w/ d095000

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,33 +1,22 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-function do_if_file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) else return end
-   -- require(name) results in "can't load C modules in safe
-   -- mode" error
-   wezterm.add_to_config_reload_watch_list(name); dofile(name)
-end
-
-do_if_file_exists(wezterm.config_dir.."/extkeys.lua")
-do_if_file_exists(wezterm.config_dir.."/specific.lua")
-
 keys = {
    -- emacs undo. C- don't need to be unbound, prefer s- prefix for WM
-   {key = "_",         mods = "CTRL|SHIFT",     action = "DisableDefaultAssignment"},
-   {key = "-",         mods = "CTRL|SHIFT",     action = "DisableDefaultAssignment"},
-   {key = "-",         mods = "CTRL",           action = "DisableDefaultAssignment"},
-   {key = "=",         mods = "CTRL",           action = "DisableDefaultAssignment"},
+   {key = '_',         mods = 'CTRL|SHIFT',     action = 'DisableDefaultAssignment'},
+   {key = '-',         mods = 'CTRL|SHIFT',     action = 'DisableDefaultAssignment'},
+   {key = '-',         mods = 'CTRL',           action = 'DisableDefaultAssignment'},
+   {key = '=',         mods = 'CTRL',           action = 'DisableDefaultAssignment'},
    -- emacs just-one-space
-   {key = "Enter",     mods = "ALT",            action = "DisableDefaultAssignment"},
+   {key = 'Enter',     mods = 'ALT',            action = 'DisableDefaultAssignment'},
    -- panes
-   {key = "UpArrow",   mods = "CTRL|SHIFT",     action = "DisableDefaultAssignment"},
-   {key = "DownArrow", mods = "CTRL|SHIFT",     action = "DisableDefaultAssignment"},
-   {key = "%",         mods = "SHIFT|ALT|CTRL", action = "DisableDefaultAssignment"},
-   {key = "o",         mods = "SUPER",          action = act.ActivatePaneDirection 'Next',},
-   {key = "i",         mods = "SUPER",          action = act.ActivatePaneDirection 'Prev',},
-   {key = "-",         mods = "CTRL|SUPER",     action = act.SplitVertical{   domain = 'CurrentPaneDomain' },},
-   {key = "\\",        mods = "CTRL|SUPER",     action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' },},
+   {key = 'UpArrow',   mods = 'CTRL|SHIFT',     action = 'DisableDefaultAssignment'},
+   {key = 'DownArrow', mods = 'CTRL|SHIFT',     action = 'DisableDefaultAssignment'},
+   {key = '%',         mods = 'SHIFT|ALT|CTRL', action = 'DisableDefaultAssignment'},
+   {key = 'o',         mods = 'SUPER',          action = act.ActivatePaneDirection 'Next',},
+   {key = 'i',         mods = 'SUPER',          action = act.ActivatePaneDirection 'Prev',},
+   {key = '-',         mods = 'CTRL|SUPER',     action = act.SplitVertical{   domain = 'CurrentPaneDomain' },},
+   {key = '\\',        mods = 'CTRL|SUPER',     action = act.SplitHorizontal{ domain = 'CurrentPaneDomain' },},
    {key = 'z',         mods = 'CTRL|SUPER',     action = wezterm.action.TogglePaneZoomState,},
    {key = 'UpArrow',   mods = 'ALT|SUPER',      action = act.AdjustPaneSize { 'Up',   1 },},
    {key = 'DownArrow', mods = 'ALT|SUPER',      action = act.AdjustPaneSize { 'Down', 1 },},
@@ -35,31 +24,7 @@ keys = {
    {key = 'DownArrow', mods = 'CTRL|SUPER',     action = act.AdjustPaneSize { 'Down', 5 },},
 
    -- copy mode, a la C-Space from tmux
-   {key = "Space",     mods = "CTRL|SUPER",     action = act.ActivateCopyMode },
-
-   {
-      -- key = "P", pane select -- TODO: B??
-      key = "B",
-      mods = "CTRL",
-      action = wezterm.action.QuickSelectArgs {
-         label = "open url",
-         patterns = {
-            '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-            '\\bb/[0-9]+\\S*\\b',
-            '\\bcl/[0-9]+\\S*\\b',
-            '\\bgo/\\S+\\b',
-            '\\bgoogle3/[\\w./-]+',
-         },
-         action = wezterm.action_callback(function(window, pane)
-               local url = window:get_selection_text_for_pane(pane)
-               if not string.match(url, '^https?://') then
-                  url = 'http://' .. url
-               end
-               wezterm.log_info("opening: " .. url)
-               wezterm.open_with(url)
-         end),
-      },
-   },
+   {key = 'Space',     mods = 'CTRL|SUPER',     action = act.ActivateCopyMode },
 }
 
 -- emacs copy-mode binds
@@ -85,62 +50,20 @@ for _, v in pairs({
       { key = 's', mods = 'CTRL', action = act.CopyMode 'NextMatch' },
       { key = 'r', mods = 'CTRL', action = act.CopyMode 'PriorMatch'},
       { key = 'g', mods = 'CTRL', action = act.CopyMode 'Close' },
-      { key = "k", mods = "CTRL", action = act.CopyMode 'ClearPattern' },
+      { key = 'k', mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
 }) do
    table.insert(search_mode, v)
 end
 
--- to click links, need both shift (bypass_mouse_reporting_modifiers = true) +
--- ctrl (mouse_reporting = false). somehow both are needed in tmux (mouse mode)
--- + emacs (xterm-mouse-mode)
-mouse_bindings = {
-   {
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
-      action = act.OpenLinkAtMouseCursor,
-      -- allowing wezterm clicks to bypass tmux/emacs mouse captures:
-      mouse_reporting = true,
-   },
-}
-
--- TODO: make these default
-hyperlink_rules = {
-   {
-      regex = "\\bhttps?://\\S*\\b",
-      format = "$0",
-   },
-   { -- This is actually the default if you don't specify any hyperlink_rules
-      regex = "\\b\\w+://(?:[\\w.-]+)\\.[a-z]{2,15}\\S*\\b",
-      format = "$0",
-   },
-   { -- linkify email addresses
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
-   },
-   { -- file:// URI
-      regex = "\\bfile://\\S*\\b",
-      format = "$0",
-   }
-}
-
--- https://stackoverflow.com/questions/1283388/lua-how-to-merge-two-tables-overwriting-the-elements-which-are-in-both
-if keys_extended_shortcuts then
-   for k,v in pairs(keys) do keys_extended_shortcuts[k] = v end
-end
-if hyperlink_rules_specific then
-   for k,v in pairs(hyperlink_rules_specific) do hyperlink_rules[k] = v end
-end
-
-return {
+local config = {
    -- text & colours
-   font_size = font_size,
-   font = wezterm.font("Iosevka"),
-   color_scheme = "GruvboxDark",
-   -- color_scheme = "Gruvbox Light",
+   font = wezterm.font('Iosevka'),
+   color_scheme = 'GruvboxDark',
+   -- color_scheme = 'Gruvbox Light',
 
    -- cursor
    default_cursor_style = 'BlinkingBlock',
-   cursor_thickness = "1.4pt",
+   cursor_thickness = '1.4pt',
    hide_mouse_cursor_when_typing = false,
 
    -- keys and links
@@ -155,7 +78,7 @@ return {
    -- window
    window_background_opacity = 0.9,
    hide_tab_bar_if_only_one_tab = true,
-   window_close_confirmation = "NeverPrompt",
+   window_close_confirmation = 'NeverPrompt',
    enable_scroll_bar = true,
 
    -- domains
@@ -168,3 +91,7 @@ return {
    -- etc
    warn_about_missing_glyphs = false,
 }
+
+local specific = require 'specific'
+specific.apply_to_config(config)
+return config
