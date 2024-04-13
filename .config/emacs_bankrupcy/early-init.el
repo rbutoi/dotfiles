6ca37a4ceb36094e41d33544b39cffa4d78b18d2
Radu Butoi <rbutoi@gmail.com>
2024-04-13 18:17:42 -0400

    bankrupt emacs again, start with elpaca
    since recent emacsen, `emacs --init-directory ~/.config/emacs_bankrupcy`

---
+++ .config/emacs_bankrupcy/early-init.el
@@ -0,0 +1,1 @@
+(setq package-enable-at-startup nil)	; https://github.com/progfolio/elpaca?tab=readme-ov-file#installer
---
+++ .config/emacs_bankrupcy/init.el
@@ -0,0 +1,1 @@
+(load (expand-file-name "lisp/elpaca-init.el" user-emacs-directory))
