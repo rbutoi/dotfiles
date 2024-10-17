236a884bdfd530cb113a730be1da873e0fa07dd6
Radu Butoi <Radu.Butoi@cardlytics.com>
2024-10-17 14:57:51 -0400

    lrn2read docs, use `#search-query -- -.`
    https://github.com/minad/consult/blob/afda4c68a4ebcc9104bf2a645848d7e92269f054/consult.el#L4860-L4862
    
    also -. == --hidden

--- .config/emacs/lisp/custom.el
+++ .config/emacs/lisp/custom.el
@@ -12,13 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(consult-fd-args
-   '((if
-         (executable-find "fdfind" 'remote)
-         "fdfind" "fd")
-     "--full-path --color=never" "--hidden"))
- '(consult-ripgrep-args
-   "rg --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --with-filename --line-number --search-zip --hidden")
  '(custom-safe-themes
    '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
  '(warning-suppress-types '((emacs))))
