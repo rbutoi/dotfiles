cd763b541ebd6f817ee8b0aa338f6753bf4629ce
Radu Butoi <rbutoi@gmail.com>
2025-01-08 11:25:33 -0500

    more macOS skhd, emacs magit fixes + s-binds, gruvbox + kitty
    also back to gmail

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -23,8 +23,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("b9c002dc827fb75b825da3311935c9f505d48d7ee48f470f0aa7ac5d2a595ab2" "9d8d60e69db09062e759f008e59fac1d76618b09cddaf06cef093653c16f1e75" default))
- '(safe-local-variable-values '((elisp-lint-indent-specs (git-gutter:awhen . 1)))))
+   '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "b9c002dc827fb75b825da3311935c9f505d48d7ee48f470f0aa7ac5d2a595ab2" "9d8d60e69db09062e759f008e59fac1d76618b09cddaf06cef093653c16f1e75" default))
+ '(safe-local-variable-values
+   '((eval when
+           (fboundp 'rainbow-mode)
+           (rainbow-mode 1))
+     (elisp-lint-indent-specs
+      (git-gutter:awhen . 1)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
