387e4b624b2bcaa27da001a6927df8fde506e545
Radu Butoi <rbutoi@gmail.com>
2025-11-13 17:30:41 -0500

    .

--- .config/emacs/lisp/custom.el
+++ .config/emacs/lisp/custom.el
@@ -6,7 +6,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default)))
+   '("0592f1b03ba5d7d8cfade2ce3a23db4c0d5f9926c9ae918dd740739dd95b697c"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
