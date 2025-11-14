52e4c5e2f4f3456bb341d94a48193b42a5f0715e
Radu Butoi <rbutoi@gmail.com>
2025-11-14 16:04:31 -0500

    treemacs back on. 0.01 seconds was too small
    + ccb646464d6a447bf1971c44b1c6cdefe14347fc, and https://github.com/alphapapa/defrepeater.el/issues/3

--- .config/emacs/lisp/custom.el
+++ .config/emacs/lisp/custom.el
@@ -7,7 +7,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("0592f1b03ba5d7d8cfade2ce3a23db4c0d5f9926c9ae918dd740739dd95b697c"
-     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default)))
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default))
+ '(warning-suppress-log-types '((emacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
