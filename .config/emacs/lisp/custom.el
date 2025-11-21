944f53e8f8a0d696d826cf43d47ec62e0e7fe8ef
Radu Butoi <rbutoi@gmail.com>
2025-11-21 16:19:03 -0500

    visual upgrade

--- .config/emacs/lisp/custom.el
+++ .config/emacs/lisp/custom.el
@@ -6,7 +6,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7e98dc1aa7f5db0557691da690c38d55e83ddd33c6d268205d66e430d57fb982"
+   '("166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da"
+     "993aac313027a1d6e70d45b98e121492c1b00a0daa5a8629788ed7d523fe62c1"
+     "c0d59357271d17e33e7b875e030e17431b2f20098dcfdc1081fa09b62da7f963"
+     "7e98dc1aa7f5db0557691da690c38d55e83ddd33c6d268205d66e430d57fb982"
      "0592f1b03ba5d7d8cfade2ce3a23db4c0d5f9926c9ae918dd740739dd95b697c"
      "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default))
  '(safe-local-variable-values '((checkdoc-allow-quoting-nil-and-t . t)))
