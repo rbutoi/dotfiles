56d5d10aab50afc65f10da14c630a6a45b14d2d5
Radu Butoi <rbutoi@gmail.com>
2026-04-06 11:41:29 -0400

    trying kitty for better copy mode? hmm

--- .config/emacs/lisp/custom.el
+++ .config/emacs/lisp/custom.el
@@ -13,7 +13,9 @@
      "166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da"
      "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" default))
  '(safe-local-variable-values
-   '((lisp-indent-local-overrides (cond . 0) (interactive . 0))
+   '((eval add-hook 'after-save-hook
+           (lambda nil (shell-command "pkill -USR1 -a kitty")) nil :local)
+     (lisp-indent-local-overrides (cond . 0) (interactive . 0))
      (checkdoc-allow-quoting-nil-and-t . t)))
  '(warning-suppress-log-types
    '((copilot copilot-exceeds-max-char) (copilot copilot-no-mode-indent)
